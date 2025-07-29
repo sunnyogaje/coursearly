05e6b3125e2c932f87d2afdc5e9c9ecdb21ec0d6
Abba <sunnyogaje@gmail.com>
2025-07-29 22:16:32 +0100

    Update

---
+++ coursearly.sql
@@ -0,0 +1,695 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 29, 2025 at 11:16 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coursearly`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `affiliate_course`
+--
+
+CREATE TABLE `affiliate_course` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `clicks` int(11) NOT NULL,
+  `purchases` int(11) NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `approved_earning`
+--
+
+CREATE TABLE `approved_earning` (
+  `id` int(11) NOT NULL,
+  `course_purchase_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banks`
+--
+
+CREATE TABLE `banks` (
+  `id` int(11) NOT NULL,
+  `bank` varchar(500) NOT NULL,
+  `code` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `category` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `certificate_template`
+--
+
+CREATE TABLE `certificate_template` (
+  `id` int(11) NOT NULL,
+  `html_code` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `course_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `course_title` longtext NOT NULL,
+  `course_subtitle` longtext NOT NULL,
+  `course_description` longtext NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `subcategory_id` int(11) NOT NULL,
+  `promotional_video` longtext NOT NULL,
+  `level_id` int(11) NOT NULL,
+  `language_id` int(11) NOT NULL,
+  `course_tags` longtext NOT NULL,
+  `currency_id` int(11) NOT NULL,
+  `course_price` float NOT NULL,
+  `course_tot_students` int(11) NOT NULL,
+  `course_earnings` float NOT NULL,
+  `course_clicks` int(11) NOT NULL,
+  `course_purchases` int(11) NOT NULL,
+  `course_status` int(11) NOT NULL,
+  `course_reject_description` longtext NOT NULL,
+  `course_rating` int(11) NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_max_price`
+--
+
+CREATE TABLE `course_max_price` (
+  `id` int(11) NOT NULL,
+  `currency_id` int(11) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_ownership_duration`
+--
+
+CREATE TABLE `course_ownership_duration` (
+  `id` int(11) NOT NULL,
+  `duration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_purchase`
+--
+
+CREATE TABLE `course_purchase` (
+  `id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `affiliate_percent` int(11) NOT NULL,
+  `company_percent` int(11) NOT NULL,
+  `instructor_percent` int(11) NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `currency`
+--
+
+CREATE TABLE `currency` (
+  `currency_id` int(11) NOT NULL,
+  `currecy` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `languages`
+--
+
+CREATE TABLE `languages` (
+  `id` int(11) NOT NULL,
+  `language` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level`
+--
+
+CREATE TABLE `level` (
+  `id` int(11) NOT NULL,
+  `course_level` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modes_of_verification`
+--
+
+CREATE TABLE `modes_of_verification` (
+  `id` int(11) NOT NULL,
+  `mode_of_verification` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modules`
+--
+
+CREATE TABLE `modules` (
+  `modules_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `module_name` varchar(1000) NOT NULL,
+  `video_link` longtext NOT NULL,
+  `text_content` longtext NOT NULL,
+  `file_attached` longtext NOT NULL,
+  `module_position` int(11) NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pending_earning`
+--
+
+CREATE TABLE `pending_earning` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `course_purchase_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `duration` int(11) NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_approval_duration`
+--
+
+CREATE TABLE `purchase_approval_duration` (
+  `id` int(11) NOT NULL,
+  `usertype` varchar(500) NOT NULL,
+  `duration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quizz`
+--
+
+CREATE TABLE `quizz` (
+  `quizz_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `questions` longtext NOT NULL,
+  `options` longtext NOT NULL,
+  `answer` longtext NOT NULL,
+  `explanation` longtext NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quizz_results`
+--
+
+CREATE TABLE `quizz_results` (
+  `id` int(11) NOT NULL,
+  `quizz_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `student_answer` longtext NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `rating_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `rating_1_to_5` int(11) NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+CREATE TABLE `review` (
+  `review_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `module_id` int(11) NOT NULL,
+  `comment` longtext NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rules_and_regulations`
+--
+
+CREATE TABLE `rules_and_regulations` (
+  `id` int(11) NOT NULL,
+  `usertype` varchar(500) NOT NULL,
+  `rules` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_courses`
+--
+
+CREATE TABLE `student_courses` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `course_module_id` int(11) NOT NULL,
+  `course_start_date` varchar(500) NOT NULL,
+  `course_end_date` varchar(500) NOT NULL,
+  `course_done_status` int(11) NOT NULL,
+  `course_payment_end_date` varchar(500) NOT NULL,
+  `course_certificate` longtext NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subcategory`
+--
+
+CREATE TABLE `subcategory` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `subcategory` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_tbl`
+--
+
+CREATE TABLE `users_tbl` (
+  `user_id` int(11) NOT NULL,
+  `full_name` varchar(500) NOT NULL,
+  `user_type` varchar(500) NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `wallet_balance` float NOT NULL,
+  `mode_of_verification` int(11) NOT NULL,
+  `uploaded_document` longtext NOT NULL,
+  `verification_video` longtext NOT NULL,
+  `withdrawal_bank` int(11) NOT NULL,
+  `withdrawal_acc_no` int(11) NOT NULL,
+  `withdrawal_acc_name` varchar(1000) NOT NULL,
+  `status` int(11) NOT NULL,
+  `date` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `withdrawal_history`
+--
+
+CREATE TABLE `withdrawal_history` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `affiliate_course`
+--
+ALTER TABLE `affiliate_course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `approved_earning`
+--
+ALTER TABLE `approved_earning`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `banks`
+--
+ALTER TABLE `banks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `certificate_template`
+--
+ALTER TABLE `certificate_template`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `course_max_price`
+--
+ALTER TABLE `course_max_price`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `course_ownership_duration`
+--
+ALTER TABLE `course_ownership_duration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `course_purchase`
+--
+ALTER TABLE `course_purchase`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `currency`
+--
+ALTER TABLE `currency`
+  ADD PRIMARY KEY (`currency_id`);
+
+--
+-- Indexes for table `languages`
+--
+ALTER TABLE `languages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `level`
+--
+ALTER TABLE `level`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `modes_of_verification`
+--
+ALTER TABLE `modes_of_verification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `modules`
+--
+ALTER TABLE `modules`
+  ADD PRIMARY KEY (`modules_id`);
+
+--
+-- Indexes for table `pending_earning`
+--
+ALTER TABLE `pending_earning`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `purchase_approval_duration`
+--
+ALTER TABLE `purchase_approval_duration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `quizz`
+--
+ALTER TABLE `quizz`
+  ADD PRIMARY KEY (`quizz_id`);
+
+--
+-- Indexes for table `quizz_results`
+--
+ALTER TABLE `quizz_results`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`rating_id`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`review_id`);
+
+--
+-- Indexes for table `rules_and_regulations`
+--
+ALTER TABLE `rules_and_regulations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_courses`
+--
+ALTER TABLE `student_courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_tbl`
+--
+ALTER TABLE `users_tbl`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `withdrawal_history`
+--
+ALTER TABLE `withdrawal_history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `affiliate_course`
+--
+ALTER TABLE `affiliate_course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `approved_earning`
+--
+ALTER TABLE `approved_earning`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `banks`
+--
+ALTER TABLE `banks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `certificate_template`
+--
+ALTER TABLE `certificate_template`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `course_max_price`
+--
+ALTER TABLE `course_max_price`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `course_ownership_duration`
+--
+ALTER TABLE `course_ownership_duration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `course_purchase`
+--
+ALTER TABLE `course_purchase`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `currency`
+--
+ALTER TABLE `currency`
+  MODIFY `currency_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `languages`
+--
+ALTER TABLE `languages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `level`
+--
+ALTER TABLE `level`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `modes_of_verification`
+--
+ALTER TABLE `modes_of_verification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `modules`
+--
+ALTER TABLE `modules`
+  MODIFY `modules_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pending_earning`
+--
+ALTER TABLE `pending_earning`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `purchase_approval_duration`
+--
+ALTER TABLE `purchase_approval_duration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quizz`
+--
+ALTER TABLE `quizz`
+  MODIFY `quizz_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quizz_results`
+--
+ALTER TABLE `quizz_results`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `rating_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rules_and_regulations`
+--
+ALTER TABLE `rules_and_regulations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `student_courses`
+--
+ALTER TABLE `student_courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users_tbl`
+--
+ALTER TABLE `users_tbl`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `withdrawal_history`
+--
+ALTER TABLE `withdrawal_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
